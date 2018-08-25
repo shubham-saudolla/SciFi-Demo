43928063a70f707cbb9f30cd379a1eabe32d7ac7
Shubham Saudolla <shubhamsaudolla@gmail.com>
2018-08-26 00:03:35 +0530

    Added the mouse look in horizontal direction

---
+++ SciFi Demo/Assets/Game/Scripts/LookX.cs
@@ -0,0 +1,29 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float _mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += (_mouseX * _sensitivity);
+
+        transform.localEulerAngles = newRotation;
+    }
+}
