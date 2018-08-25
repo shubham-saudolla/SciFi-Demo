60f0e1ff05afdedd54591ddb0c4b953f4cd01d92
Shubham Saudolla <shubhamsaudolla@gmail.com>
2018-08-25 22:56:33 +0530

    Set up basic player movement

---
+++ SciFi Demo/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // as this is a 3D game we only move in x and z axis and y axis is for gravity
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(direction * Time.deltaTime);
+    }
+}
