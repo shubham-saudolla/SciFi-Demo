9eb3f62d32b44b8c9816adccffcd0603afde0832
Shubham Saudolla <shubhamsaudolla@gmail.com>
2018-08-26 00:08:59 +0530

    Set up the first person character controller

---
+++ SciFi Demo/Assets/Game/Scripts/LookY.cs
@@ -0,0 +1,29 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x -= (_mouseY * _sensitivity);
+
+        transform.localEulerAngles = newRotation;
+    }
+}
